7aeadfddab75f1208c340d08b5839e442d4b73b2
Flemming Jønsson <frj@trifork.com>
2012-11-08 22:07:17 +0100

    Added license and some description to the script

--- scripts/resign_ipa.sh
+++ scripts/resign_ipa.sh
@@ -1,5 +1,38 @@
 #!/bin/bash
 
+#**************************************************************************
+#   Copyright (C) 2012, Flemming Jønsson                                  *
+#                                                                         *
+#   This program is free software; you can redistribute it and/or modify  *
+#   it under the terms of the GNU General Public License as published by  *
+#   the Free Software Foundation; either version 2 of the License, or     *
+#   (at your option) any later version.                                   *
+#                                                                         *
+#   This program is distributed in the hope that it will be useful,       *
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+#   GNU General Public License for more details.                          *
+#                                                                         *
+#   You should have received a copy of the GNU General Public License     *
+#   along with this program; if not, write to the                         *
+#   Free Software Foundation, Inc.,                                       *
+#   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+#**************************************************************************
+
+
+
+#**************************************************************************
+# This script can be used to re-sign an Apple ipa file for OTA            *
+# distribution.                                                           *
+#                                                                         *
+# Usage:                                                                  *
+# resign_ipa.sh <some_ipa.ipa> <some_profile.mobileprovision>             *
+#                                                                         *
+# 	Note: Code signing identity, bundle identifiers etc. will be extracted*
+#         from the provisioning profile.                                  *
+#**************************************************************************
+
+
 if [ $# -lt 2 ]; then
   echo "Usage: $0 <IPA_FILE> <PROVISIONING_FILE>"
   exit 1
@@ -35,24 +68,26 @@ mkdir -p "${WORKDIR}"
 
 OUTPUT_IPA_FILE="${IPA_FILE%.ipa}.resigned.ipa"
 
+
+
 ###################### BEGIN FUNCTIONS ######################
 function plist_from_mobileprovision()
 {
 	
 	tmp_file="/tmp/tmp.plist"
-	local input_file="${1}"
+	
+	if [ ! -f "${1}" ]; then
+  		echo "Mobileprovision file '${1}' not found"
+  		exit 1
+  	fi
 
 	## get the plist embedded in the provisioning profile
 	# 1. sed - Get contents between <?xml.... xml </plist> both tags inclusive
 	# 2. sed - If any content is left in xml prolog, delete it
 	# 3 xmllist - just to make the file nicer to look at
-	plist_xml=$(strings "$input_file" | sed -n "/<?xml*/,/<\/plist>/p" | sed "s/^.*\(<?.*>\)/\1/g;s/date>/string>/g")
+	plist_xml=$(strings "${1}" | sed -n "/<?xml*/,/<\/plist>/p" | sed "s/^.*\(<?.*>\)/\1/g;s/date>/string>/g")
 	$(echo ${plist_xml} | xmllint --format - > "$tmp_file")
-	if [ -n "${DEBUG}" ]; then
-		cat "${tmp_file}"
-	fi
-	echo "${tmp_file}"
-	
+	echo "${tmp_file}"	
 }
 
 function code_signing_identity_from_plist()
