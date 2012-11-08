dfbc78719e6c9535ff4522b04f9cbffd21222bc7
Flemming Jønsson <frj@trifork.com>
2012-11-08 23:14:21 +0100

    Fixed problem with relative paths

--- scripts/resign_ipa.sh
+++ scripts/resign_ipa.sh
@@ -41,43 +41,38 @@
 
 
 if [ $# -lt 2 ]; then
-  echo "Usage: $0 <IPA_FILE> <PROVISIONING_FILE>"
+  echo "Usage: $0 <IPA_FILE> <PROVISIONING_FILE> [OUTPUTDIR]"
+  echo "   Parameters:"
+  echo "      1: IPA file to re-sign."
+  echo "      2: MobileProvision file to use for re-signing."
+  echo "     [3]: Optional. Output directory for the re-signed IPA file."
+  echo "   If parameter 3 is not specified the default is current dir"
   exit 1
 fi
 
 
-if [ ! -f "${1}" ]; then
-  echo "File '$1' not found"
-  exit 1
-else
-  IPA_FILE="${1}"
-fi
 
-if [ ! -f "${2}" ]; then
-  echo "File '$2' not found"
-  exit 1
-else
-	MOBILE_PROVISION="$2"
-fi
-
-
-echo ""
-echo "Re-signing ipa with content from mobileprovision"
-echo "  IPA:             '${IPA_FILE}'"
-echo "  MobileProvision: '${MOBILE_PROVISION}'"
-echo ""
-
-CURDIR=$(pwd)
-
-WORKDIR="/tmp/resign"
+WORKDIR="${TMPDIR}/resign"
 rm -rf "${WORKDIR}"
 mkdir -p "${WORKDIR}"
 
-OUTPUT_IPA_FILE="${IPA_FILE%.ipa}.resigned.ipa"
 
 
 
 ###################### BEGIN FUNCTIONS ######################
+
+function failed()
+{
+    local error=${1:-Undefined error}
+    echo "Failed: $error" >&2
+    exit 1
+}
+
+function absolute_path()
+{
+  python -c "import os.path; print os.path.abspath('$1')"
+}
+
 function plist_from_mobileprovision()
 {
 	
@@ -108,10 +103,10 @@ function code_signing_identity_from_plist()
   ## Extract certificate from mobileprovision plist
   local developer_certificates
   developer_certificates=$(/usr/libexec/PlistBuddy -c 'print :DeveloperCertificates:0' "$1")  
-  echo $developer_certificates > "tmp_cert"
+  echo $developer_certificates > "${WORKDIR}/tmp_cert"
 
   local iphone_dist
-  iphone_dist=$(strings "tmp_cert" | grep "iPhone Distribution")
+  iphone_dist=$(strings "${WORKDIR}/tmp_cert" | grep "iPhone Distribution")
   iphone_dist=${iphone_dist:2}
 
   local iphone_dist_length
@@ -141,21 +136,57 @@ function bundle_identifier_from_plist()
 ###################### END FUNCTIONS ######################
 
 
+### Check input ###
+if [ ! -f "${1}" ]; then
+  failed "File '$1' not found"
+else
+  IPA_FILE=$(absolute_path "${1}")
+fi
+
+if [ ! -f "${2}" ]; then
+  failed "File '$2' not found"
+else
+  MOBILE_PROVISION=$(absolute_path "${2}")
+fi
+
+if [ $# -gt 2 ]; then
+  if [ -d "${3}" ]; then
+    OUTDIR=$(absolute_path "${3}")
+  else 
+    echo "Output dir '${3}' does not exist, falling back to current dir"
+    OUTDIR="$(pwd)"
+  fi
+else
+  OUTDIR="$(pwd)"
+fi
+
+IPA_FILENAME_ONLY=$(basename "${IPA_FILE}")
+OUTPUT_IPA_FILE="${IPA_FILENAME_ONLY%.ipa}.resigned.ipa"
+
+echo ""
+echo "Re-signing ipa with content from mobileprovision"
+echo "  IPA:             '${IPA_FILE}'"
+echo "  MobileProvision: '${MOBILE_PROVISION}'"
+echo ""
+echo "  Result:          '${OUTDIR}/${OUTPUT_IPA_FILE}'"
+echo ""
+
+
+
+
 #Extract the plist from the .mobileprovision file
 provisioning_plist_file=$(plist_from_mobileprovision "${MOBILE_PROVISION}")
 
 #Extract BUNDLE_IDENTIFIER from plist
 BUNDLE_IDENTIFIER=$(bundle_identifier_from_plist "$provisioning_plist_file")
 if [ -z "${BUNDLE_IDENTIFIER}" ]; then
-	echo "Failed fetching BUNDLE_IDENTIFIER from '$provisioning_plist_file'"
-	exit 1
+	failed "Failed fetching BUNDLE_IDENTIFIER from '$provisioning_plist_file'"
 fi
 
 #Extract CODE_SIGNING_IDENTITY from plist
 CODE_SIGNING_IDENTITY=$(code_signing_identity_from_plist "$provisioning_plist_file")
 if [ -z "${CODE_SIGNING_IDENTITY}" ]; then
-	echo "Failed fetching CODE_SIGNING_IDENTITY from '$provisioning_plist_file'"
-	exit 1
+	failed "Failed fetching CODE_SIGNING_IDENTITY from '$provisioning_plist_file'"
 fi
 
 #Extract uuid and expiration_date from plist
@@ -178,8 +209,7 @@ expiration_date_epoch=$(date -jf "%Y-%m-%dT%H:%M:%SZ" "$expiration_date" +%s)
 today_epoch=$(date +%s)
 
 if [ "${today_epoch}" -ge "${expiration_date_epoch}" ]; then
-	echo "The mobile provisioning file has expired: $expiration_date  ---> $expiration_date_epoch  ---> $(date -r $expiration_date_epoch)"
-	exit 1
+	failed "The mobile provisioning file has expired: $expiration_date  ---> $expiration_date_epoch  ---> $(date -r $expiration_date_epoch)"
 fi
 
 
@@ -200,20 +230,20 @@ rm -r "Payload/${APP_NAME}.app/_CodeSignature" "Payload/${APP_NAME}.app/CodeReso
 
 #Replace embedded mobile provisioning profile
 echo "Embedding mobileprovision in app"
-cp "${MOBILE_PROVISION}" "Payload/${APP_NAME}.app/embedded.mobileprovision"
+cp "${MOBILE_PROVISION}" "Payload/${APP_NAME}.app/embedded.mobileprovision" || failed "Failed copying mobileprovision to app dir"
 
 
 ## Set the bundleidentifier in Info.plist to match 
-echo "CFBundleIdentifier: '${BUNDLE_IDENTIFIER}' (${MOBILE_PROVISION})" 
-/usr/libexec/PlistBuddy -c 'Set :CFBundleIdentifier "${BUNDLE_IDENTIFIER}"' "Payload/${APP_NAME}.app/Info.plist"
+echo "CFBundleIdentifier: '${BUNDLE_IDENTIFIER}' ($(basename ${MOBILE_PROVISION}))" 
+/usr/libexec/PlistBuddy -c 'Set :CFBundleIdentifier "${BUNDLE_IDENTIFIER}"' "Payload/${APP_NAME}.app/Info.plist" || failed "Setting CFBundleIdentifier failed"
 
 #Re-sign
-echo "Code signing identity: '${CODE_SIGNING_IDENTITY}' (${MOBILE_PROVISION})"
-/usr/bin/codesign -f -s "${CODE_SIGNING_IDENTITY}" --resource-rules "Payload/${APP_NAME}.app/ResourceRules.plist" "Payload/${APP_NAME}.app"
+echo "Code signing identity: '${CODE_SIGNING_IDENTITY}' ($(basename ${MOBILE_PROVISION}))"
+/usr/bin/codesign -f -s "${CODE_SIGNING_IDENTITY}" --resource-rules "Payload/${APP_NAME}.app/ResourceRules.plist" "Payload/${APP_NAME}.app" || failed "Code signing failed"
 
 #Re-package
 echo "Packaging IPA file"
-zip -qr "${CURDIR}/${OUTPUT_IPA_FILE}" Payload
+zip -qr "${OUTDIR}/${OUTPUT_IPA_FILE}" Payload || failed "Unable to package ipa"
 
 echo ""
 echo "DONE - produced '${OUTPUT_IPA_FILE}'"
